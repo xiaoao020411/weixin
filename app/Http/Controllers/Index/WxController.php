8f066fadbe744f71f0c717b4f0b0e9ce7f633d30
changshenao <1253689006@qq.com>
2020-11-06 14:03:45 +0800

    access_token

--- app/Http/Controllers/Index/WxController.php
+++ app/Http/Controllers/Index/WxController.php
@@ -34,25 +34,25 @@ class WxController extends Controller
     }
     //获取access_token
     public function getAccessToken(){
-        $key = 'wx:access_token';
-        $token = Redis::get($key);
-        if($token){
-            echo "有缓存";
-        }else{
-            echo "无缓存";
+        
+        //$token = Redis::get($key);
+        // if($token){
+        //     echo "有缓存";
+        // }else{
+        //     echo "无缓存";
         $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".env('WX_APPID')."&secret=".env('WX_APPSEC');
         $response = file_get_contents($url);
         
         $data = json_decode($response,true);
         $token = $data['access_token'];
-
+        $key = 'wx:access_token';
         
         Redis::set($key,$token);
-        Redis::expire($key,3600);
+        Redis::expire($key,3600         );
         echo "access_token:",$token;
         }
         
 
         
     }
-}
+
