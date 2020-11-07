b760c4669bd728b3ecb4d40fef232dd8caa19650
changshenao <1253689006@qq.com>
2020-11-07 14:40:19 +0800

    短信

--- app/Http/Controllers/Index/WxController.php
+++ app/Http/Controllers/Index/WxController.php
@@ -71,54 +71,28 @@ class WxController extends Controller
                 $data = simplexml_load_string($xml_data, 'SimpleXMLElement', LIBXML_NOCDATA);
                 if($data->MsgType=='event'){
                     if($data->Event=='subscribe'){
-                        $openid = $data->FromUserName;
-                        $AccessToken=$this->getAccessToken();
-                        $url = $url="https://api.weixin.qq.com/cgi-bin/user/info?access_token=".$AccessToken."&openid=".$openid."&lang=zh_CN";
-                        $user = file_get_contents($url);
-                        $user = json_decode($user,true);
-                        if(isset($user['errcode'])){
-                            $this->writeLog("获取用户失败");
-                        }else{
-                            if(!Redis::get($openid)){
-                                Redis::set($openid,'ggg');
-                                $content= "谢谢你关注";
-                                echo $this->xml($data,$content);
-                            }else{
-                                $content= "谢谢你再次关注我";
-                                echo $this->xml($data,$content);
-                            }
-                        }
+                        $ToUserName=$data->FromUserName;
+                        $FromUserName=$data->ToUserName;
+                        $CreateTime=time();
+                        $MsgType="text";
+                        $array=['欢迎关注'];
+                        $Content=$array[array_rand($array)];
+                        $xml="<xml>
+                                <ToUserName><![CDATA[%s]]></ToUserName>
+                                <FromUserName><![CDATA[%s]]></FromUserName>
+                                <CreateTime>%s</CreateTime>
+                                <MsgType><![CDATA[%s]]></MsgType>
+                                <Event><![CDATA[%s]]></Event>
+                                <Content><![CDATA['.$Content.']]></Content>
+                            </xml>";
+                            echo sprintf($xml,$ToUserName,$FromUserName,$CreateTime,$MsgType,$Content);
                     }
-                    if($data->Event=='unsubscribe'){
-
-                    }
-                }
+                        
             }else{
                 echo "";
             }
         }
-        public function xml($data,$content){
-            $ToUserName=$data->FromUserName;
-            $FromUserName=$data->ToUserName;
-            $CreateTime=time();
-            $MsgType="text";
-            $xml="<xml>
-                    <ToUserName><![CDATA[%s]]></ToUserName>
-                    <FromUserName><![CDATA[%s]]></FromUserName>
-                    <CreateTime>%s</CreateTime>
-                    <MsgType><![CDATA[%s]]></MsgType>
-                    <Event><![CDATA[%s]]></Event>
-                </xml>";
-                echo sprintf($xml,$ToUserName,$FromUserName,$CreateTime,$MsgType,$content);
-        }
-
-
-        public function writeLog($data){
-            if(is_object($data) || is_array($data)){
-                $data = json_encode($data);
-            }
-            file_put_contents('weixin2004.txt',$data);
-        }
-
+        
     }
 
+}
