50b9ddb12c7a2715ffda5a35bab1d6cdfd99ee3f
changshenao <1253689006@qq.com>
2020-11-10 20:08:17 +0800

    1

--- resources/views/test/huizi.blade.php
+++ resources/views/test/huizi.blade.php
@@ -8,5 +8,12 @@
 </head>
 <body>
     <img src="/static/photo/7665d881963cb580f5850dc09f8cb34.jpg" style="widows: 100px; height:100px;" alt="">
+    <img src="/static/photo/f7e028dd238dbca29ff562a0c0211ad.jpg" style="widows: 100px; height:100px;" alt=""><br>
+    <img src="/static/photo/051f8f605fed173b74c17f0293c12a8.jpg" style="widows: 100px; height:100px;" alt="">
+    <img src="/static/photo/071c984f4abe14ae5f2562021c5831a.jpg" style="widows: 100px; height:100px;" alt=""><br>
+    <img src="/static/photo/d4c4f366212f0e8731b000fd8002ae6.jpg" style="widows: 100px; height:100px;" alt="">
+    <img src="/static/photo/d57fc3b87f168ac661701e60ff1700c.jpg" style="widows: 100px; height:100px;" alt=""><br>
+    <img src="/static/photo/50d4e89932f6fc35cb722b855413cba.jpg" style="widows: 100px; height:100px;" alt="">
+    <img src="/static/photo/87d47539d6b8181a8ba31626d8cd6a8.jpg" style="widows: 100px; height:100px;" alt=""><br>
 </body>
 </html>
